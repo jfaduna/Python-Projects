a6df19c540a4c952d8f0214d3503f6b3d0b9e562
jfaduna <flomeraduna@gmail.com>
2024-01-23 22:03:43 +0800

    add: Quiz Project

---
+++ Quiz Project/main.py
@@ -0,0 +1,12 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = [Question(q['text'], q['answer']) for q in question_data]
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
